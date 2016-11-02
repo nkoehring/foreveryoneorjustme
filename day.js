62c9d22f2ee4707a7e66fe8ec61127c619776db6
Norman Köhring <norman.koehring@fromatob.com>
2016-11-02 19:13:37 +0100

    remove spinner, timeapi.org redirects to foxmovies without www?!

--- day.js
+++ day.js
@@ -1,18 +1,35 @@
-function isItTheDay (date, day, msg) {
+function setMessage (date) {
   const el = document.getElementById('msg')
   const el2 = document.getElementById('sorry')
+  const config = window.config
+  const itIsTheDay = date.getDay() === config.day
 
-  if ((new Date()).getDay() === day) {
-    el.className = 'bad'
-    el.innerText = msg.good
-    el2.innerText = msg.sorry
+  el.innerText = ''
+  el2.innerText = ''
+
+  if (itIsTheDay) {
+    el.className = 'yes'
+    el.innerText = config.messages.yes
+    if (config.sorryIf) {
+      el2.innerText = config.messages.sorry
+    }
   } else {
-    el.className = 'good'
-    el.innerText = msg.bad
-    el2.innerText = '' 
+    el.className = 'no'
+    el.innerText = config.messages.no
+    if (!config.sorryIf) {
+      el2.innerText = config.messages.sorry
+    }
   }
 }
 
 function apiCallback (json) {
-  const date = new Date(json.dateString);
+  let date = new Date()
+
+  if (json && json.dateString) {
+    date = new Date(json.dateString)
+  }
+
+  setMessage(date)
 }
+
+window.apiCallback = apiCallback
